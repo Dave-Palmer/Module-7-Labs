f42b24359977297ba59b0e8b08cd0a2f0c211d80
Dave-Palmer <djarmor@gmail.com>
2023-03-28 16:21:22 +1300

    added navigate to add new student page

--- vite-react-project/src/components/StudentCard.jsx
+++ vite-react-project/src/components/StudentCard.jsx
@@ -6,28 +6,9 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 import { Link } from "react-router-dom";
 import CustomizedDialogs from './CustomDialogBox';
-// import { SimpleDialog } from './SimpleDialogDemo';
-
-// function Student({ id, name, location, set = 'set1' }) { //destructure the props object into variables, and optionally set default values
-
-//     return (
-//         <li className="Student"><img src={`https://robohash.org/${name}?size=50x50&set=${set}`} alt={name} />
-//             <span className="details"><Link to={"/students/" + id}><strong>{name}</strong></Link>, based in {location}</span></li>
-//     )
-// }
-
-// export default Student
-
 
 
 export default function StudentCard({ id, name, location, set = 'set1', description }) {
-    const handleClickOpen = () => {
-        setOpen(true);
-    };
-    const handleClose = () => {
-        setOpen(false);
-    };
-
 
     return (
         <Card sx={{ maxWidth: 300 }}>
@@ -40,7 +21,6 @@ export default function StudentCard({ id, name, location, set = 'set1', descript
                     alt={name}
                 // onClick={() => alert('Hello')}
                 />
-
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         {name}
